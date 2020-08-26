c8a49bcc825cfdc291f4c71a2ae13230cfa1973b
danie <danielrgc97@gmail.com>
2020-08-26 11:37:33 +0200

    Agregado plugin file sin debacle

--- src/app/main-timers/main-timers.page.ts
+++ src/app/main-timers/main-timers.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Caja } from './caja.model';
 import { CajasService } from './cajas.service';
-import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
 
-const { Filesystem } = Plugins;
 
 @Component({
   selector: 'app-main-timers',
@@ -13,25 +11,12 @@ const { Filesystem } = Plugins;
 export class MainTimersPage implements OnInit {
   cajas: Caja[];
 
-  constructor(private cajasService: CajasService) { }
+  constructor( private cajasService: CajasService) { }
 
   ngOnInit() {
     this.cajas = this.cajasService.getAllCajas();
   }
 
-  async fileWrite() {
-    try {
-      const result = await Filesystem.writeFile({
-        path: 'secrets/text.txt',
-        data: "This is a test",
-        directory: FilesystemDirectory.ExternalStorage,
-        encoding: FilesystemEncoding.UTF8
-      })
-      console.log('@Wrote file', result);
-    } catch(e) {
-      console.error('@Unable to write file', e);
-    }
+  createFile(){
   }
-
-
 }
