e4143f45b2e7c5f3e99e92357918765a00972749
danie <danielrgc97@gmail.com>
2020-08-26 14:50:18 +0200

    Solucionando "directory dont exist"

--- src/app/main-timers/main-timers.page.ts
+++ src/app/main-timers/main-timers.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Caja } from './caja.model';
 import { CajasService } from './cajas.service';
 
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-main-timers',
@@ -11,12 +12,16 @@ import { CajasService } from './cajas.service';
 export class MainTimersPage implements OnInit {
   cajas: Caja[];
 
-  constructor( private cajasService: CajasService) { }
+  constructor(private file: File, private cajasService: CajasService) { }
 
   ngOnInit() {
     this.cajas = this.cajasService.getAllCajas();
   }
 
   createFile(){
+
+    this.file.checkDir(this.file.dataDirectory, 'mydir').then(_ => console.log('Directory exists')).catch(err =>
+      console.log('Directory doesnt exist'));
+
   }
 }
