14e6fe44953b87a6f8fe8ece8a89e2cdf9577fed
danie <danielrgc97@gmail.com>
2020-08-26 21:39:33 +0200

    Creacion de ficheros funcionando, creo

--- src/app/main-timers/main-timers.page.ts
+++ src/app/main-timers/main-timers.page.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Caja } from './caja.model';
 import { CajasService } from './cajas.service';
 
-import { File } from '@ionic-native/file/ngx';
+import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
+
+const { Filesystem } = Plugins;
+const { Storage } = Plugins;
 
 @Component({
   selector: 'app-main-timers',
@@ -11,17 +14,46 @@ import { File } from '@ionic-native/file/ngx';
 })
 export class MainTimersPage implements OnInit {
   cajas: Caja[];
+  jsonString: string;
 
-  constructor(private file: File, private cajasService: CajasService) { }
+  constructor( private cajasService: CajasService) {}
 
   ngOnInit() {
     this.cajas = this.cajasService.getAllCajas();
   }
 
-  createFile(){
 
-    this.file.checkDir(this.file.dataDirectory, 'mydir').then(_ => console.log('Directory exists')).catch(err =>
-      console.log('Directory doesnt exist'));
-
+  testing(){
+    console.log();
   }
+
+  // async fileWrite() {
+  //   try {
+  //     const result = await Filesystem.writeFile({
+  //       path: '/secrets/text.txt',
+  //       data: "This is a test",
+  //       directory: FilesystemDirectory.Data,
+  //       recursive: true,
+  //       encoding: FilesystemEncoding.UTF8
+  //     })
+  //     console.log('Wrote file', result);
+  //   } catch(e) {
+  //     console.error('Unable to write file', e);
+  //   }
+  // }
+
+  // async setObject() {
+  //   await Storage.set({
+  //     key: '8Timer',
+  //     value: JSON.stringify(
+  //     )
+  //   });
+  // }
+
+  // // JSON "get" example
+  // async getObject() {
+  //   const ret = await Storage.get({ key: '8Timer' });
+  //   const user = JSON.parse(ret.value);
+  // }
+
 }
